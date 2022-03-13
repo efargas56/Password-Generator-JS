20018fdb7c0bb42d70891d6d11a34739e6786819
ef56 <efargas56@gmail.com>
2022-03-13 15:41:44 -0400

    changed if else code added notes

--- Develop/script.js
+++ Develop/script.js
@@ -11,35 +11,50 @@ function writePassword() {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
+// password variable types
 var upperCaseConfirm = window.confirm("Would you like to use uppercase letters?")
 var lowerCaseConfirm = window.confirm("Would you like to use lowercase letters?")
 var numbersConfirm = window.confirm("would you like to use numbers?")
 var specialCharConfirm = window.confirm("would you like to use special characters?")
-
+ // constant letters and symbols
 const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const numbers = "1234567890"
 const specialChar =" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-
+ // if and else functions
 if (upperCaseConfirm && lowerCaseConfirm && numbersConfirm && specialCharConfirm){
   var randomCharacter = function() {
     var character = (upperCase + lowerCase + numbers + specialChar)[Math.floor(Math.random()* (upperCase.length + lowerCase.length + numbers.length + specialChar.length))]
     return character
   }
-  else if (lowerCaseConfirm && numbersConfirm && specialCharConfirm){
+   } else if (lowerCaseConfirm && numbersConfirm && specialCharConfirm){
     var randomCharacter = function(){
-      var character = (lowerCase + numbers + specialChar)[Math.floor(math.random()* (lowerCase.length + numbers.length + specialChar.length))]
+      var character = (lowerCase + numbers + specialChar)[Math.floor(Math.random()* (lowerCase.length + numbers.length + specialChar.length))]
       return character
     }
-  else if (upperCaseConfirm && lowerCaseConfirm && numbersConfirm){
+   } else if (upperCaseConfirm && lowerCaseConfirm && numbersConfirm){
     var randomCharacter = function() {
-      var character = (upperCase + lowerCase + numbers)[math.floor(math.random() * (upperCase.length + numbers.length + specialChar.length))]
+      var character = (upperCase + lowerCase + numbers)[Math.floor(Math.random() * (upperCase.length + numbers.length + specialChar.length))]
       return character
     }
-  else if (upperCaseConfirm && numbersConfirm && specialCharConfirm)
-   }
-
-
+   } else if (upperCaseConfirm && numbersConfirm && specialCharConfirm){
+    var randomCharacter = function() {
+     var character = (upperCase + numbers + specialChar)[Math.floor(Math.random() * (upperCase.length + numbers.length + specialChar.length))]
+    return character
+    }
+   } else if (upperCaseConfirm && lowerCaseConfirm && specialCharConfirm){
+    var randomCharacter = function() {
+      var character = (lowerCase + upperCase + specialChar)[Math.floor(Math.random() * (lowerCase.length + upperCase.length + specialChar.length))]
+     return character
+    }
+   } else if (upperCaseConfirm && lowercaseconfirm){
+     var randomCharacter = function() {
+       var character = (upperCase + lowerCase)[Math.floor(Math.random() * (upperCase.length + lowerCase.length))] 
+     }
+  } else  (upperCaseConfirm)
+    var randomCharacter = function() {
+      var character = (upperCase)[Math.floor(Math.random() * (upperCase.length))]
+    }
+  
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", randomCharacter);
